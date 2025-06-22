2132003b46d2c1e868a5321fe1b9019af84d8557
Kubek <jakub.michnowicz@gmail.com>
2025-06-22 15:04:28 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
+  vite: {
+      plugins: [tailwindcss()]
+  },
+  integrations: [react()],
   adapter: netlify(),
 });
